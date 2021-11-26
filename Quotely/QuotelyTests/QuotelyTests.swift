7b3441351e3c909c44a60e66f2451de6fba882d9
Morgan <nihao0705@gmail.com>
2021-11-26 11:48:06 +0800

    Create target of unit test

--- Quotely/QuotelyTests/QuotelyTests.swift
+++ Quotely/QuotelyTests/QuotelyTests.swift
@@ -6,27 +6,19 @@
 //
 
 import XCTest
+@testable import Quotely
 
 class QuotelyTests: XCTestCase {
 
+    var sut: SwipeViewController!
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = SwipeViewController()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
