277fe63a5e575b833b97bac8509716a05d0824ba
Morgan <nihao0705@gmail.com>
2021-12-06 17:13:04 +0800

    Remove redundant codes

--- Quotely/Quotely/AppDelegate.swift
+++ Quotely/Quotely/AppDelegate.swift
@@ -11,10 +11,6 @@ import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
-
-    static let shared = UIApplication.shared.delegate as! AppDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
